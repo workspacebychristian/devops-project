26475ea9cde2401986166e0aa47e9bd2d50200c6
workspacebychuka@gmail.com <workspacebychuka@gmail.com>
2024-08-30 15:14:24 +0000

    k

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
